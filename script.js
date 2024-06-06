ed1b4c9db093d39d600a629c300d2658e72ccfb6
FabianoCordioli <106283423+FabianoCordioli@users.noreply.github.com>
2024-06-05 21:10:25 -0300

    Create script.js 05/06

---
+++ script.js
@@ -0,0 +1,22 @@
+const QuadroPrincipal = document.querySelector(".caixa-principal");
+const boxPerguntas = document.querySelector(".caixa-perguntas");
+const Alternativas = document.querySelector(".caixa-alternativas");
+const boxResultado = document.querySelector(".caixa-resultado");
+const Resultado = document.querySelector(".texto-resultado");
+
+const Perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]; 

fe621425bda8ec9c028369d4a38a8625b3234aef
FabianoCordioli <106283423+FabianoCordioli@users.noreply.github.com>
2024-06-27 17:54:51 -0300

    Create export-perguntas.js

---
+++ export-perguntas.js
@@ -0,0 +1,88 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [ 
+                "afirmacao",
+                ""
+            ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                "afirmacao"
+            ]
+            }
+        ]
+    },    
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [ 
+                "afirmacao"
+            ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                "afirmacao"
+            ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [ 
+                "afirmacao"
+            ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [ 
+                "afirmacao"
+            ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [ 
+                "afirmacao"
+            ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [
+                "afirmacao"
+            ]
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [ 
+                "afirmacao"
+            ]
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: [ 
+                "afirmacao"
+            ]
+            }
+        ]
+    },
+];

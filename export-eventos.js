4045364b6c10bb5300ddb3cda61fd16abbc65904
Fabrício <106283423+FabianoCordioli@users.noreply.github.com>
2024-06-27 18:15:19 -0300

    Update export-eventos.js

--- export-eventos.js
+++ export-eventos.js
@@ -1,4 +1,8 @@
+const nomes = ["Fabrício", "Janjan"];
+
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
